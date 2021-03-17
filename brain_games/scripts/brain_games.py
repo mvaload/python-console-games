e6e0902a67e8eedbe2879bb41fc07f828d361638
mvaload <mvaload@gmail.com>
2021-03-17 23:03:27 +0300

    completed third step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+from brain_games.scripts import cli
 
 def main():
     print('Welcome to the Brain Games!')
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
